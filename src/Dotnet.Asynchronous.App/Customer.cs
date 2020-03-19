6a0fbbfe4b4b61b29fa1a56bc90ad6e192072dd3
Lex Galante <alex_gm_100@hotmail.com>
2020-03-18 23:12:18 -0300

    *

--- src/Dotnet.Asynchronous.App/Customer.cs
+++ src/Dotnet.Asynchronous.App/Customer.cs
@@ -9,5 +9,7 @@ namespace Dotnet.Asynchronous.App
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public override string ToString() => string.Format("Id: {0}, Name: {1}, Age: {2}", Id, Name, Age);
     }
 }
--- src/Dotnet.Asynchronous.App/Program.cs
+++ src/Dotnet.Asynchronous.App/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,6 +15,8 @@ namespace Dotnet.Asynchronous.App
         {
             try
             {
+                Console.WriteLine($"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}] Inicio programa");
+
                 ExamplePlinq();
                 Thread.Sleep(1000);
                 ExampleSingleThread();
@@ -25,6 +30,17 @@ namespace Dotnet.Asynchronous.App
                 ExampleCancelationToken();
                 Thread.Sleep(1000);
                 ExampleTaskContinueWith();
+                Thread.Sleep(1000);
+                ExampleParallelFor();
+                Thread.Sleep(1000);
+                ExampleThreadPool();
+                Thread.Sleep(1000);
+                ExampleKeywordAsyncAndAwait();
+                Thread.Sleep(1000);
+                ExampleThreadSafe();
+
+                Console.WriteLine($"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}] Fim do programa");
+
                 Console.ReadKey();
             }
             catch (Exception e)
@@ -36,7 +52,7 @@ namespace Dotnet.Asynchronous.App
 
         private static void ExamplePlinq()
         {
-            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
             Console.WriteLine("Exemplo PLINQ");
 
             var customers = new List<Customer>() 
@@ -47,6 +63,8 @@ namespace Dotnet.Asynchronous.App
             };
 
             var parallelSearch = from customer in customers.AsParallel()
+                                                           .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                                                           .WithDegreeOfParallelism(2)
                                  where customer.Age >= 18
                                  select customer;
 
@@ -67,11 +85,14 @@ namespace Dotnet.Asynchronous.App
             combineParallelSearch.ForEach((customer) => Console.WriteLine($"{customer.Id} - {customer.Name} - {customer.Age}"));
 
 
-            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
         
         private static void ExampleSingleThread()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo SINGLE THREAD");
+
             var thread1 = new Thread(new ThreadStart(() => 
             {
                 var nums = Enumerable.Range(0, 10);
@@ -80,10 +101,17 @@ namespace Dotnet.Asynchronous.App
             }));
 
             thread1.Start();
+
+            Parallel.Invoke(() => Console.WriteLine("single thread"));
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
 
         private static void ExampleBackgroundSingleThread()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo BACKGROUND SINGLE THREAD");
+
             var thread2 = new Thread(new ThreadStart(() =>
             {
                 var nums = Enumerable.Range(0, 10);
@@ -93,10 +121,15 @@ namespace Dotnet.Asynchronous.App
 
             thread2.IsBackground = true;
             thread2.Start();
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
 
         private static void ExampleGetThreadResult()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo GET THREAD RESULT");
+
             var task1 = Task.Run(() => 
             {
                 var x = new Random().Next(1000, 9999);
@@ -107,10 +140,14 @@ namespace Dotnet.Asynchronous.App
             });
 
             var result = task1.GetAwaiter().GetResult();
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
 
         private static void ExampleMultiTasks()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo MULTI THREADS");
+
             var source = new CancellationTokenSource();
             var cancellationToken = source.Token;
             var tasks = new Task[]
@@ -131,10 +168,14 @@ namespace Dotnet.Asynchronous.App
                 if (sort == 1)
                     source.Cancel();
             }
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
 
         private static void ExampleCancelationToken()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo CANCELATION TOKEN");
+
             var source = new CancellationTokenSource();
             var cancellationToken = source.Token;
             var tasks = new Task[]
@@ -151,6 +192,7 @@ namespace Dotnet.Asynchronous.App
                 task.Start();
 
             source.Cancel();
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
 
         private static void ExecuteTask(CancellationToken token, int number)
@@ -166,6 +208,9 @@ namespace Dotnet.Asynchronous.App
 
         private static void ExampleTaskContinueWith()
         {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo CONTINUE WITH");
+
             var task = Task.Factory.StartNew(() =>
             {
                 Console.WriteLine($"taskContinueWith running...");
@@ -175,10 +220,79 @@ namespace Dotnet.Asynchronous.App
             task.ContinueWith(task => 
             {
                 Console.WriteLine($"taskContinueWith can be finished? {task.Status.ToString()}");
-                Console.ReadKey();
             });
 
             task.Wait();
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+        }
+
+        private static void ExampleParallelFor()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo CONTINUE WITH");
+
+            var json = File.ReadAllText("customers.json");
+            var customers = JsonSerializer.Deserialize<List<Customer>>(json);
+            Parallel.ForEach(customers, customer => Console.WriteLine($"Pararel iterarion"));
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+        }
+
+        private static void ExampleThreadPool()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo THREAD POOL");
+
+            ThreadPool.QueueUserWorkItem((object customer) =>
+            {
+                Console.WriteLine((customer as Customer).ToString());
+            }, new Customer() { Id = 9999, Name = "Cledovaldo", Age = 2 });
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+        }
+
+        private static void ExampleKeywordAsyncAndAwait()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo KEYWORDS ASYNC AWAIT");
+
+            var source = new CancellationTokenSource();
+            var cancellationToken = source.Token;
+            var task = GetCustomersAsync(cancellationToken);
+            var customers = task.GetAwaiter().GetResult();
+            Console.WriteLine($"GetCustomersAsync.Count = {customers.Count}");
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+        }
+
+        private static async Task<List<Customer>> GetCustomersAsync(CancellationToken cancellationToken)
+        {
+            using var stream = new StreamReader("customers.json");
+
+            return await JsonSerializer.DeserializeAsync<List<Customer>>(stream.BaseStream);
+        }
+
+        private static void ExampleThreadSafe()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exemplo THREAD SAFE");
+            
+            var source = new CancellationTokenSource();
+            var cancellationToken = source.Token;
+            var customers = GetCustomersAsync(cancellationToken).GetAwaiter().GetResult();
+            var queue = new ConcurrentQueue<Customer>();
+            Console.WriteLine("add customer in concurrent");
+            Parallel.Invoke(
+                () => customers.ForEach((customer) => queue.Enqueue(customer)),
+                () => customers.ForEach((customer) => queue.Enqueue(customer)),
+                () => customers.ForEach((customer) => queue.Enqueue(customer)),
+                () => customers.ForEach((customer) => queue.Enqueue(customer))
+            );
+            if (queue.TryPeek(out Customer customer))
+                Console.WriteLine($"concurrent queue peek: {customer.ToString()}");
+            Console.WriteLine($"finished invoke customers in concurrent queue total: {queue.Count}");
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
         }
     }
 }
